4c029a735edd7a844ba8cb0b04fdc9e5b104edaa
Rishabh Garg <rishabh@greendeck.co>
2020-09-22 19:25:50 +0530

    designed cart page

--- server.js
+++ server.js
@@ -5,18 +5,20 @@ const path = require('path')
 const expressEjsLayouts = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3000
 
+// set templating engine, explictly telling express to use below configurations
+app.use(expressEjsLayouts)
+app.set('views', path.join(__dirname,"/resources/views/"))
+app.set("view engine",'ejs')
+
 //serving resources on requested routes ASSETS
 app.use(express.static('public'))
 app.get("/", (req,res)=> {
     res.render('home')
     // res.send("Hi there from server");
 })
-
-// set templating engine, explictly telling express to use below configurations
-app.use(expressEjsLayouts)
-app.set('views', path.join(__dirname,"/resources/views/"))
-app.set("view engine",'ejs')
-
+app.get("/cart", (req,res)=> {
+    res.render('customers/cart.ejs')
+})
 // server listening on PORT
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
